21c300128d8609661b49a79e859a802cba0c56e1
Marko Vucinic <marko.vucinic@clarivate.com>
2023-12-08 10:09:33 +0100

    Solve Day 2 part 2 in Rust

--- rust/src/day2/mod.rs
+++ rust/src/day2/mod.rs
@@ -41,14 +41,44 @@ pub fn run() {
             .collect(),
     );
 
-    let result = calculate_part_1(&games);
-    println!("Day 2, part 1: {}", result);
+    let result_part_1 = calculate_part_1(&games);
+    println!("Day 2, part 1: {}", result_part_1);
+
+    let result_part_2 = calculate_part_2(&games);
+    println!("Day 2, part 2: {}", result_part_2);
 }
 
 fn calculate_part_1(games: &Vec<Game>) -> u32 {
     games
         .iter()
         .map(|game| if game.is_possible() { game.id } else { 0 })
+        .sum()
+}
+
+fn calculate_part_2(games: &Vec<Game>) -> u32 {
+    games
+        .iter()
+        .map(|game| {
+            let mut red: u32 = 0;
+            let mut green: u32 = 0;
+            let mut blue: u32 = 0;
+
+            game.sets.iter().for_each(|set| {
+                if set.red > red {
+                    red = set.red;
+                }
+
+                if set.green > green {
+                    green = set.green;
+                }
+
+                if set.blue > blue {
+                    blue = set.blue;
+                }
+            });
+
+            red * green * blue
+        })
         .sum()
 }
 
@@ -136,4 +166,16 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
     let input: Vec<Game> = transform_input(input_text.split("\n").map(String::from).collect());
     assert_eq!(calculate_part_1(&input), 8);
+}
+
+#[test]
+fn test_calculate_part_2() {
+    let input_text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    let input: Vec<Game> = transform_input(input_text.split("\n").map(String::from).collect());
+    assert_eq!(calculate_part_2(&input), 2286);
 }
